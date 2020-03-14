2791c5704ca79a4f8b7e47b93b86459bebf29e0c
fizik003 <kassik0boks@gmail.com>
2020-03-14 11:18:48 +0300

    init: test_socket

---
+++ templates.py
@@ -0,0 +1,8 @@
+main = b"""
+<h2>hello main page</h2>
+<img src="https://im0-tub-ru.yandex.net/i?id=87554897624001e4b2d2333ffbfb9eac&n=33&w=188&h=188" alt = 'img'>
+"""
+
+shop = b'''
+<h1>hello shop page</h1>
+'''
